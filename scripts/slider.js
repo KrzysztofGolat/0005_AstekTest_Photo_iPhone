570c417124e43585b235c2391114c4deb5d6f786
KrzysztofGolat <kgolat@gmail.com>
2016-02-25 01:35:04 +0100

    Version for small displays.

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,6 +1,12 @@
+var wysokosc = $('.slider').height();
+var szerokosc = $('.slider').width();
+
+$('.slider').css({height: wysokosc + 'px'});
+$('.slide').css({width: szerokosc + 'px', position: 'absolute'});
+
 $('.prev').click(
     function() {
-        $('.active').animate({left: '100%'}, 1000, function(){
+        $('.active').animate({left: '100%'}, 600, function(){
             $(this).removeClass('active');
             $(this).css({left: '0'});
         });
@@ -12,6 +18,24 @@ $('.prev').click(
         
         console.log($poprzedniSlajd);
         
-        $poprzedniSlajd.css({left: '-100%' }).addClass('active').animate({ left: '0' }, 1000);
+        $poprzedniSlajd.css({left: '-100%' }).addClass('active').animate({ left: '0' }, 600);
+    }
+)
+
+$('.next').click(
+    function() {
+        $('.active').animate({right: '100%'}, 600, function(){
+            $(this).removeClass('active');
+            $(this).css({left: '0'});
+        });
+        
+        var $nastepnySlajd = $('.active').next('.slide');
+        if(!$nastepnySlajd.length) { 
+            $nastepnySlajd = $('.active').nextAll('.slide').first();
+        }
+        
+        console.log($nastepnySlajd);
+        
+        $nastepnySlajd.css({left: '-100%' }).addClass('active').animate({ left: '0' }, 600);
     }
 )
