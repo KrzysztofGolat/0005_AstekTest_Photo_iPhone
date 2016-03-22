03d83be36965dff316336ad386d856ce92f4b800
KrzysztofGolat <kgolat@gmail.com>
2016-03-22 18:25:46 +0100

    Slider fixed.

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,41 +1,45 @@
-var wysokosc = $('.slider').height();
-var szerokosc = $('.slider').width();
+$(document).ready(function () {
 
-$('.slider').css({height: wysokosc + 'px'});
-$('.slide').css({width: szerokosc + 'px', position: 'absolute'});
+    var wysokosc = $('.slider').height();
+    var szerokosc = $('.slider').width();
 
-$('.prev').click(
-    function() {
-        $('.active').animate({left: '100%'}, 600, function(){
-            $(this).removeClass('active');
-            $(this).css({left: '0'});
-        });
-        
-        var $poprzedniSlajd = $('.active').prev('.slide');
-        if(!$poprzedniSlajd.length) { 
-            $poprzedniSlajd = $('.active').nextAll('.slide').last();
+    $('.slider').css({height: wysokosc + 'px'});
+    $('.slide').css({width: szerokosc + 'px', position: 'absolute'});
+
+    $('.prev').click(
+        function() {
+            $('.active').animate({left: '100%'}, 600, function(){
+                $(this).removeClass('active');
+                $(this).css({left: '0'});
+            });
+
+            var $poprzedniSlajd = $('.active').prev('.slide');
+            if(!$poprzedniSlajd.length) { 
+                $poprzedniSlajd = $('.active').nextAll('.slide').last();
+            }
+
+            console.log($poprzedniSlajd);
+
+            $poprzedniSlajd.css({left: '-100%' }).addClass('active').animate({ left: '0' }, 600);
         }
-        
-        console.log($poprzedniSlajd);
-        
-        $poprzedniSlajd.css({left: '-100%' }).addClass('active').animate({ left: '0' }, 600);
-    }
-)
+    )
 
-$('.next').click(
-    function() {
-        $('.active').animate({right: '100%'}, 600, function(){
-            $(this).removeClass('active');
-            $(this).css({left: '0'});
-        });
-        
-        var $nastepnySlajd = $('.active').next('.slide');
-        if(!$nastepnySlajd.length) { 
-            $nastepnySlajd = $('.active').nextAll('.slide').first();
+    $('.next').click(
+        function() {
+            $('.active').animate({right: '100%'}, 600, function(){
+                $(this).removeClass('active');
+                $(this).css({left: '0'});
+            });
+
+            var $nastepnySlajd = $('.active').next('.slide');
+            if(!$nastepnySlajd.length) { 
+                $nastepnySlajd = $('.active').nextAll('.slide').first();
+            }
+
+            console.log($nastepnySlajd);
+
+            $nastepnySlajd.css({left: '-100%' }).addClass('active').animate({ left: '0' }, 600);
         }
-        
-        console.log($nastepnySlajd);
-        
-        $nastepnySlajd.css({left: '-100%' }).addClass('active').animate({ left: '0' }, 600);
-    }
-)
+    )
+    
+};
